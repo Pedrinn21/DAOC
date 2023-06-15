e2294fe43f8af2093f21cf7f2946ace03907551f
Pedro <pedropadilha15.pp@gmail.com>
2023-06-14 21:12:25 -0300

    autenticacao

---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=firefox">
+        <title>HTML5 – Estrutura básica</title>
+    </head>
+
+    <body>
+        <div class="container">
+            @yield('content')
+
+            @auth
+            <a href="/dashboard">Lista</a>
+            <form action="/logout" method="POST">
+                @csrf
+                <a href="/logout" onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+            </form>
+            @endauth
+            @guest
+            <a href="/login">Log in</a>
+            <a href="/register">Log on</a>
+            @endguest
+        </div>
+
+    </body>
+
+</html>
