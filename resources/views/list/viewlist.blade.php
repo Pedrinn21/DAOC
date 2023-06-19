af7db08a496784d5aed039e633e0d46c9046af91
WesleyLuisDeOliveira <105400330+WesleyLuisDeOliveira@users.noreply.github.com>
2023-06-19 01:01:04 -0300

    Update viewlist.blade.php
    Correção do anterior

--- resources/views/list/viewlist.blade.php
+++ resources/views/list/viewlist.blade.php
@@ -7,8 +7,6 @@
         <div class="container">
         @foreach($lista as $list)
             <div class="card">
-            <h2> {{$list->nome}}</h2>
-               <br>
         
             <h1>{{$list->nome}}</h1>
         
@@ -18,20 +16,17 @@
             <p class="card-content">
                 <h4> Informação do Lixo </h5>
             {{Str::limit($list->infolixo, 200, '...') }}
-            <h2>Características</h2>
-            {{$list->carac}}
-
             <br>
             <br>
             <h4> Precauções ao descartar</h4>
             {{Str::limit($list->descarte, 200, '...') }}
                 </p>
-                <h5 class="align-right"><a href="http://127.0.0.1:8000/search?search={{ urlencode($list->nome) }}" >mais informações </a></h5>
+                <h5 class="align-right"><a href="/search?search={{ urlencode($list->nome) }}" >mais informações </a></h5>
             </div>
             <br>
             <br>
             <div>
-                <a type="button" href="{{ route ('delete.list', ['list' => $list->list_id])}}">Desfavoritar</a>
+                
 
             </div>
 
